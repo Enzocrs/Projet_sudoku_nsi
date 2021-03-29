f4cb6702bd6297abc84c9ea17ac648a256724f59
Enzocrs <81557838+Enzocrs@users.noreply.github.com>
2021-03-29 20:19:42 +0200

    Add files via upload
    Mise à jour, après cours du 29/03/21.

---
+++ sudoku_main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+
+"""
+
+Ce module est le module principal.
+Il contient seulement le squelette du projet
+Il est le seul à contenir du code directement executable.
+
+Les autres modules contiendront:
+    Des définitions de fonctions
+    Des classes
+    Des asserts
+    etc.
+mais aucun code directement executable
+
+
+Les auteurs de ce projet sont:
+    * Enzo Croses    TGEN 2
+    * Hugo Leroux    TGEN 2
+
+Version du 30/03/2021
+"""
+
+if __name__ == "__main__":
+    pass
