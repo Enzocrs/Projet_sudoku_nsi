691c035fc377004ba3a4594e1bffeb1358005e40
Enzocrs <81557838+Enzocrs@users.noreply.github.com>
2021-03-30 16:45:41 +0200

    Update class_sudoku.py
    petite màj

--- class_sudoku.py
+++ class_sudoku.py
@@ -1,3 +1,10 @@
-
-
+from random import randint
 class Sudoku:
+    """
+    Créer la classe principale qui permettra de creer et d'intéragir avec un jeu
+    """
+  
+    def __init__(self):
+        
+        
+    
