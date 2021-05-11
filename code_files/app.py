c75c31317e000a1edf9919d5ba0e5b3a422bfcda
Enzocrs <81557838+Enzocrs@users.noreply.github.com>
2021-05-11 12:27:03 +0200

    maj pour le cours

--- code_files/app.py
+++ code_files/app.py
@@ -4,17 +4,34 @@ Version du 10 Mai 2021
 Enzo CROSES
 """
 import sqlite3
+import data
+
+a = data.dico_grilles["grille_1"]
+print(a)
 
 # Connexion à la base de données et association du curseur à la variable "c"
 conn = sqlite3.Connection("database.db")
 c = conn.cursor()
 
+# Création du tableau qui gère les grilles
+
 c.execute("""
 CREATE TABLE IF NOT EXISTS Grilles(
     Id_Grille INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
     Grille TEXT
-)""")
+)
+""")
 
+# Création du tableau qui gère les joueurs
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS Joueurs(
+    Id_Joueur INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    Pseudo TEXT,
+    Password TEXT,
+    Grilles_jouées TEXT
+)
+""")
 
 
 
---
+++ code_files/data.py
@@ -0,0 +1,19 @@
+"""
+Ce programme a pour seul but de stocker les données utilisées dans app.py pour que son code soit plus lisible
+Enzo CROSES
+Version du 10/05/2021
+"""
+dico_grilles = {
+    
+    "grille_1" : [
+        [6,7,5,8,4,9,2,1,3],
+        [2,4,1,5,6,3,9,8,7],
+        [9,3,8,2,7,1,4,6,5],
+        [5,9,7,1,3,6,8,2,4],
+        [8,1,4,9,2,5,7,3,6],
+        [3,2,6,4,8,7,5,9,1],
+        [7,5,3,6,9,8,1,4,2],
+        [1,8,2,3,5,4,6,7,9],
+        [4,6,9,7,1,2,3,5,8]
+    ]
+}
