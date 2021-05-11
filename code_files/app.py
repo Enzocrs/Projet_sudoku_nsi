f59fdd003fa4c7cf681628848aed84592ca031e3
Enzocrs <81557838+Enzocrs@users.noreply.github.com>
2021-05-11 22:17:48 +0200

    Maj soir 11/05/2021
    -Script d'insertion de grilles dans app.py (fonctionnel)

--- code_files/app.py
+++ code_files/app.py
@@ -1,7 +1,7 @@
 """
 Programme lié à la base de données (sqlite3)
 Version du 11 Mai 2021
-Enzo CROSES
+Responsable: Enzo CROSES
 """
 import sqlite3
 
@@ -14,17 +14,16 @@ c = conn.cursor()
 
 c.execute("""
 CREATE TABLE IF NOT EXISTS Grilles(
-    Id_Grille INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-    ligne_1 TEXT
-    ligne_2 TEXT
-    ligne_3 TEXT
-    ligne_4 TEXT
-    ligne_5 TEXT
-    ligne_6 TEXT
-    ligne_7 TEXT
-    ligne_8 TEXT
+    Id_ligne INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    ligne_1 TEXT,
+    ligne_2 TEXT,
+    ligne_3 TEXT,
+    ligne_4 TEXT,
+    ligne_5 TEXT,
+    ligne_6 TEXT,
+    ligne_7 TEXT,
+    ligne_8 TEXT,
     ligne_9 TEXT
-
 )
 """)
 
@@ -41,26 +40,26 @@ CREATE TABLE IF NOT EXISTS Joueurs(
 """)
 
 
+# Modifier les valeurs pour insérer une nouvelle grille
+dico_grille = {
+    "ligne_1" : "765921483",
+    "ligne_2" : "983574612",
+    "ligne_3" : "412638597",
+    "ligne_4" : "248157369",
+    "ligne_5" : "571396824",
+    "ligne_6" : "396482175",
+    "ligne_7" : "639215748",
+    "ligne_8" : "127843956",
+    "ligne_9" : "854769231"
+}
 
-
-grille = [
-        [7, 6, 5, 9, 2, 1, 4, 8, 3],
-        [9, 8, 3, 5, 7, 4, 6, 1, 2],
-        [4, 1, 2, 6, 3, 8, 5, 9, 7],
-        [2, 4, 8, 1, 5, 7, 3, 6, 9],
-        [5, 7, 1, 3, 9, 6, 8, 2, 4],
-        [3, 9, 6, 4, 8, 2, 1, 7, 5],
-        [6, 3, 9, 2, 1, 5, 7, 4, 8],
-        [1, 2, 7, 8, 4, 3, 9, 5, 6],
-        [8, 5, 4, 7, 6, 9, 2, 3, 1]
-    ]
-
-def inserer_nouvelle_grille(grille):
+def inserer_nouvelle_grille(dico_grille):
 	c.execute("""
-		INSERT INTO Grilles (ligne_1) VALUES (?)""", grille
+		INSERT INTO Grilles (ligne_1, ligne_2, ligne_3, ligne_4, ligne_5, ligne_6, ligne_7, ligne_8, ligne_9)
+        VALUES (:ligne_1, :ligne_2, :ligne_3, :ligne_4, :ligne_5, :ligne_6, :ligne_7, :ligne_8, :ligne_9)"""
+        , dico_grille
 		)
 
-inserer_nouvelle_grille(grille)
-# Fermeture du fichier
+# inserer_nouvelle_grille(dico_grille)
 conn.commit()
 c.close()
--- code_files/utilitaire.py
+++ code_files/utilitaire.py
@@ -1,5 +1,5 @@
 """
-Ce programme contient uniquement des fonctions pour permettre d'accelerrer les taches longues de la programmation
+Ce programme contient uniquement des fonctions pour permettre d'accelerer les taches longues de la programmation
 """
 
 
@@ -7,9 +7,7 @@ def converti_ligne(ligne):
 	"""
 	converti 123456789 en [1, 2, 3, 4, 5, 6, 7, 8, 9]
 	"""
-	liste = []
-	for car in str(ligne):
-		liste.append(int(car))
+	liste = [int(car) for car in str(ligne)]
 	print(liste)
 
 while 1:
